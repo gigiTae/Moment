c71993c306573ad0bc4a2ab4e064e08964dc6a5b
GiTae <rlxo800@gmail.com>
2025-01-05 21:39:36 +0900

    주석 수정

--- Moment/ConsoleTest/static_constexpr.cpp
+++ Moment/ConsoleTest/static_constexpr.cpp
@@ -9,10 +9,13 @@ public:
 
 void A::Func()
 {
+	// 정적 메모리 
 	static constexpr float Min = 0.f;
 	
+	// 스택 메모리 
 	constexpr float Min2 = 0.f;
 	
+	// 스택 메모리
 	const float Min3 = 0.f;
 
 	std::cout << Min << Min2;
