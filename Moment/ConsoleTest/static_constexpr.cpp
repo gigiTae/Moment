adc8e40efdbe88fb03dd14d58ac7d65101a7f92b
이기태 <107969311+gigiTae@users.noreply.github.com>
2025-04-15 23:55:07 +0900

    d

--- Moment/ConsoleTest/static_constexpr.cpp
+++ Moment/ConsoleTest/static_constexpr.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
-
-class A
-{
-public:
-
-	void Func();
-};
-
-void A::Func()
-{
-	// 정적 메모리 
-	static constexpr float Min = 0.f;
-	
-	// 스택 메모리 
-	constexpr float Min2 = 0.f;
-	
-	// 스택 메모리
-	const float Min3 = 0.f;
-
-	std::cout << Min << Min2;
-}
-
-
-int main()
-{
-	A a;
-	A a2;
-
-
-	a.Func();
-	a2.Func();
-
-	return 0;
-}
-
+//#include <iostream>
+//
+//class A
+//{
+//public:
+//
+//	void Func();
+//};
+//
+//void A::Func()
+//{
+//	// 정적 메모리 
+//	static constexpr float Min = 0.f;
+//	
+//	// 스택 메모리 
+//	constexpr float Min2 = 0.f;
+//	
+//	// 스택 메모리
+//	const float Min3 = 0.f;
+//
+//	std::cout << Min << Min2;
+//}
+//
+//
+//int main()
+//{
+//	A a;
+//	A a2;
+//
+//
+//	a.Func();
+//	a2.Func();
+//
+//	return 0;
+//}
+//
